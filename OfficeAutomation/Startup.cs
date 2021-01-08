8d347f3c598fb7424571070889743ab74c934843
Sumit Shitole <sumitshitole1000@gmail.com>
2021-01-08 20:24:18 +0530

    Project Initilization

---
+++ OfficeAutomation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OfficeAutomation.Startup))]
+namespace OfficeAutomation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
